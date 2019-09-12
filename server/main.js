7eee645d7b463f7575c66c10c8b29c907f08e59f
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-12 18:37:19 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -41,7 +41,7 @@ function handleDisconnect() {
 
 handleDisconnect();
 
-app.get('/', function(request, response) {
+/*app.get('/', function(request, response) {
     connection.query('SELECT * from user', function(err, rows, fields) {
         if (err) {
             console.log('error: ', err);
@@ -49,7 +49,7 @@ app.get('/', function(request, response) {
         }
         response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
     });
-});
+});*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
