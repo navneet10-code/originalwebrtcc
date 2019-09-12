e5b3a0024f9181b15a66611ff4abd0f55e085293
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-12 17:39:34 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -49,7 +49,7 @@ app.get('/', function(request, response) {
             console.log('error: ', err);
             throw err;
         }
-        response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
+       // response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
     });
 });
 
